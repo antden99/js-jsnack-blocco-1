abe9d05bfa97d33d5b6cf3adbf91f57e7cdaf6b4
antden99 <antoniodenigris@icloud.com>
2024-02-23 12:16:59 +0100

    first commit

---
+++ snack-1/assets/main.js
@@ -0,0 +1,15 @@
+console.log("tutto ok");
+
+
+const input_1 = prompt("inserisci primo input");
+const input_2= prompt("inserisci secondo input");
+
+console.log(`i tuoi input sono ${input_1} e ${input_2}`)
+
+
+if (input_1 > input_2){
+    console.log("il primo input è maggiore");
+} 
+else if (input_2 > input_1){
+    console.log("il secondo input è maggiore");
+} 
